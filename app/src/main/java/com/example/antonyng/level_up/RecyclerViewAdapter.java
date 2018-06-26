0f8a17729fdcb437d83d0a8f03dc04fd12cbe3fd
meshnesh <tonnie.nesh@gmail.com>
2018-06-26 15:43:41 +0300

    [Feature] Create RecyclerViewAdapter to update recycle view layout of github username and profile pic

--- app/src/main/java/com/example/antonyng/level_up/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/antonyng/level_up/RecyclerViewAdapter.java
@@ -1,4 +1,79 @@
 package com.example.antonyng.level_up;
 
-public class RecyclerViewAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    private static final String TAG = "RecyclerViewAdapter";
+
+    private ArrayList<String> mImageNames;
+    private ArrayList<String> mImage;
+    private Context mcontext;
+
+    public RecyclerViewAdapter(Context mcontext, ArrayList<String> mImageNames, ArrayList<String> mImage) {
+        this.mImageNames = mImageNames;
+        this.mImage = mImage;
+        this.mcontext = mcontext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_listitem, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int i) {
+        Log.d(TAG, "onBindViewHolder: called");
+
+        Glide.with(mcontext)
+                .asBitmap()
+                .load(mImage.get(i))
+                .into(viewHolder.image);
+
+        viewHolder.imageName.setText(mImageNames.get(i));
+
+        viewHolder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: clicked on:" + mImageNames.get(i));
+
+                Toast.makeText(mcontext, mImageNames.get(i), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        CircleImageView image;
+        TextView imageName;
+        RelativeLayout parentLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.image);
+            imageName = itemView.findViewById(R.id.image_name);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+        }
+    }
 }
