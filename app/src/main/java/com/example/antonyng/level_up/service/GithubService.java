34e9f662bfe847733cc0b64a1372214825314b9c
meshnesh <toonnie.nesh@gmail.com>
2018-07-02 19:47:49 +0300

    [Feature] Update endpoint for accesing multiple users and single user by username

--- app/src/main/java/com/example/antonyng/level_up/service/GithubService.java
+++ app/src/main/java/com/example/antonyng/level_up/service/GithubService.java
@@ -8,10 +8,14 @@ public class GithubService {
     private Retrofit retrofit = null;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     public GithubApi getAPI() {
         String BASE_URL = "https://api.github.com/v3/";
 =======
     public GithubUsersApi getAPI() {
+=======
+    public GithubApi getAPI() {
+>>>>>>> [Feature] Update endpoint for accesing multiple users and single user by username
         String BASE_URL = "https://api.github.com/";
 >>>>>>> [Feature] Create 'GithubService' to handle api base URL && 'GithubUsersApi' to handle HTTP methods
 
@@ -24,9 +28,13 @@ public class GithubService {
         }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
         return retrofit.create(GithubApi.class);
 =======
         return retrofit.create(GithubUsersApi.class);
 >>>>>>> [Feature] Create 'GithubService' to handle api base URL && 'GithubUsersApi' to handle HTTP methods
+=======
+        return retrofit.create(GithubApi.class);
+>>>>>>> [Feature] Update endpoint for accesing multiple users and single user by username
     }
 }
