9c280df2ef6f80b247261d79e0f5facea6bfd0a3
meshnesh <toonnie.nesh@gmail.com>
2018-07-02 19:47:04 +0300

    [Chore] Refactor the file name

--- app/src/main/java/com/example/antonyng/level_up/service/GithubApi.java
+++ app/src/main/java/com/example/antonyng/level_up/service/GithubApi.java
@@ -4,10 +4,21 @@ import com.example.antonyng.level_up.model.Data;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+<<<<<<< HEAD
+=======
+import retrofit2.http.Query;
+>>>>>>> [Chore] Refactor the file name
 
 public interface GithubApi {
 
     @GET("users?")
+<<<<<<< HEAD
     Call<Data> getUserList();
 
+=======
+    Call<Data> doGetUserList(@Query("page") String page);
+
+    @GET("users/:username")
+    Call<Data> getUserResults();
+>>>>>>> [Chore] Refactor the file name
 }
