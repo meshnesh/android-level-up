374ff8c5fe26528455fb25eafa5109ae041dd459
meshnesh <toonnie.nesh@gmail.com>
2018-07-06 20:02:05 +0300

    [Chore] Fix merge conflicts

--- app/src/main/java/com/example/antonyng/level_up/presenter/GithubPresenter.java
+++ app/src/main/java/com/example/antonyng/level_up/presenter/GithubPresenter.java
@@ -1,11 +1,17 @@
 package com.example.antonyng.level_up.presenter;
 
+<<<<<<< HEAD
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 
 import com.example.antonyng.level_up.adapter.GithubUsersAdapter;
 import com.example.antonyng.level_up.model.GithubUsers;
 import com.example.antonyng.level_up.model.GithubUsersResponse;
+=======
+import com.example.antonyng.level_up.MainActivity;
+import com.example.antonyng.level_up.model.Data;
+import com.example.antonyng.level_up.model.GithubUsers;
+>>>>>>> [Feature] Create presenter package, responsible for managing data and updationg the views
 import com.example.antonyng.level_up.service.GithubService;
 
 import java.util.List;
@@ -15,6 +21,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class GithubPresenter {
+<<<<<<< HEAD
     private Context context;
 
 
@@ -42,5 +49,43 @@ public class GithubPresenter {
                 }
             }
         });
+=======
+
+    private GithubService githubService;
+    private MainActivity mainActivity;
+
+    public GithubPresenter(MainActivity view) {
+        this.mainActivity = view;
+
+        if (this.githubService == null) {
+            this.githubService = new GithubService();
+        }
+    }
+
+    public void getUserList() {
+        githubService
+                .getAPI()
+                .getUserList()
+                .enqueue(new Callback<Data>() {
+                    @Override
+                    public void onResponse(Call<Data> call, Response<Data> response) {
+                        Data data = response.body();
+
+                        if (data != null && data.getRestResponse() != null) {
+                            List<GithubUsers> result = data.getRestResponse().getResult();
+                            mainActivity.usersReady(result);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Data> call, Throwable t) {
+                        try {
+                            throw new InterruptedException("Something went wrong!");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+>>>>>>> [Feature] Create presenter package, responsible for managing data and updationg the views
     }
 }
