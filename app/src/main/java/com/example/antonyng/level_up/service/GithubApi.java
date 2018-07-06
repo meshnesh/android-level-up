173248492eec0affc12751d29078179ba14fa321
meshnesh <toonnie.nesh@gmail.com>
2018-07-06 19:37:11 +0300

    [Chore] Fix merge conflicts

--- app/src/main/java/com/example/antonyng/level_up/service/GithubApi.java
+++ app/src/main/java/com/example/antonyng/level_up/service/GithubApi.java
@@ -4,34 +4,10 @@ import com.example.antonyng.level_up.model.GithubUsersResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+ 
 import retrofit2.http.Query;
->>>>>>> [Chore] Refactor the file name
-=======
->>>>>>> [Chore] Remove unused endpoint
-
 public interface GithubApi {
 
-<<<<<<< HEAD
-    @GET("users?")
-<<<<<<< HEAD
-<<<<<<< HEAD
-    Call<Data> getUserList();
-=======
     @GET("search/users?q=language:Android+location:nairobi                                                                                                                                                                                                                                                  ")
     Call<GithubUsersResponse> getUserResults();
->>>>>>> [Chore] Get user based on location and language
-
-=======
-    Call<Data> doGetUserList(@Query("page") String page);
-
-    @GET("users/:username")
-    Call<Data> getUserResults();
->>>>>>> [Chore] Refactor the file name
-=======
-    Call<Data> getUserList();
-
->>>>>>> [Chore] Remove unused endpoint
 }
