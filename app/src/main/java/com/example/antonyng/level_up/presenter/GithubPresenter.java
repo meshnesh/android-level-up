0895c5cc2ff6dad05e5a17297e096163bd044b9d
meshnesh <toonnie.nesh@gmail.com>
2018-07-06 18:40:30 +0300

    [Feature] Change file to get data from the presenter class and pass it over to the main file

--- app/src/main/java/com/example/antonyng/level_up/presenter/GithubPresenter.java
+++ app/src/main/java/com/example/antonyng/level_up/presenter/GithubPresenter.java
@@ -1,8 +1,11 @@
 package com.example.antonyng.level_up.presenter;
 
-import com.example.antonyng.level_up.MainActivity;
-import com.example.antonyng.level_up.model.Data;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.antonyng.level_up.adapter.GithubUsersAdapter;
 import com.example.antonyng.level_up.model.GithubUsers;
+import com.example.antonyng.level_up.model.GithubUsersResponse;
 import com.example.antonyng.level_up.service.GithubService;
 
 import java.util.List;
@@ -12,41 +15,32 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class GithubPresenter {
+    private Context context;
 
-    private GithubService githubService;
-    private MainActivity mainActivity;
 
-    public GithubPresenter(MainActivity view) {
-        this.mainActivity = view;
-
-        if (this.githubService == null) {
-            this.githubService = new GithubService();
-        }
+    public GithubPresenter(Context context) {
+        this.context = context;
     }
 
-    public void getUserList() {
-        githubService
-                .getAPI()
-                .getUserList()
-                .enqueue(new Callback<Data>() {
-                    @Override
-                    public void onResponse(Call<Data> call, Response<Data> response) {
-                        Data data = response.body();
+    public void getUserList(final RecyclerView recyclerView) {
 
-                        if (data != null && data.getRestResponse() != null) {
-                            List<GithubUsers> result = data.getRestResponse().getResult();
-                            mainActivity.usersReady(result);
-                        }
-                    }
+        GithubService.getAPI().getUserResults().enqueue(new Callback<GithubUsersResponse>() {
+            @Override
+            public void onResponse(Call<GithubUsersResponse> call, Response<GithubUsersResponse> response) {
+                List<GithubUsers> users = response.body().getUsers();
+                if (users != null) {
+                    recyclerView.setAdapter(new GithubUsersAdapter(context, users));
+                }
+            }
 
-                    @Override
-                    public void onFailure(Call<Data> call, Throwable t) {
-                        try {
-                            throw new InterruptedException("Something went wrong!");
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
+            @Override
+            public void onFailure(Call<GithubUsersResponse> call, Throwable t) {
+                try {
+                    throw new InterruptedException("Something went wrong!");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
