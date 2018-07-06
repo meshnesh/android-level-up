d947e4bb02b1fb27de0bc4b29ed0262d93e8e19b
meshnesh <toonnie.nesh@gmail.com>
2018-07-06 20:02:56 +0300

    [Chore] Fix merge conflicts

--- app/src/main/java/com/example/antonyng/level_up/service/GithubApi.java
+++ app/src/main/java/com/example/antonyng/level_up/service/GithubApi.java
@@ -1,6 +1,7 @@
 package com.example.antonyng.level_up.service;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import com.example.antonyng.level_up.model.GithubUsersResponse;
 
 import retrofit2.Call;
@@ -13,14 +14,17 @@ public interface GithubApi {
     Call<GithubUsersResponse> getUserResults();
 =======
 import com.example.antonyng.level_up.model.Data;
+=======
+import com.example.antonyng.level_up.model.GithubUsersResponse;
+>>>>>>> [Chore] Get user based on location and language
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface GithubApi {
 
-    @GET("users?")
-    Call<Data> getUserList();
+    @GET("search/users?q=language:Android+location:nairobi                                                                                                                                                                                                                                                  ")
+    Call<GithubUsersResponse> getUserResults();
 
 <<<<<<< HEAD
     @GET("users/:username")
