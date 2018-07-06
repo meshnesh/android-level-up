04ad1080e095e0b213c08b75d677ccd49e47d11f
meshnesh <toonnie.nesh@gmail.com>
2018-07-06 18:44:40 +0300

    [Chore] Change data to get specific user data

--- app/src/main/java/com/example/antonyng/level_up/model/GithubUsers.java
+++ app/src/main/java/com/example/antonyng/level_up/model/GithubUsers.java
@@ -4,38 +4,21 @@ import com.google.gson.annotations.SerializedName;
 
 public class GithubUsers {
 
-    @SerializedName("id")
-    private String id;
+    @SerializedName("login")
+    private String login;
 
-    @SerializedName("name")
-    private String name;
-
-    @SerializedName("email")
-    private String email;
+    @SerializedName("avatar_url")
+    private String avatar_url;
 
     @SerializedName("company")
     private String company;
 
-    @SerializedName("bio")
-    private String bio;
-
-    public String getId() {
-        return id;
+    public String getLogin() {
+        return login;
     }
 
-    public String getName() {
-        return name;
+    public String getAvatarUrl() {
+        return avatar_url;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public String getCompany() {
-        return company;
-    }
-
-    public String getBio() {
-        return bio;
-    }
 }
