d7b7c1ce26554a197ae7a4f3b22838f0e01cbdd0
meshnesh <toonnie.nesh@gmail.com>
2018-07-06 19:49:52 +0300

    [Chore] Fix merge conflicts

--- app/src/main/java/com/example/antonyng/level_up/view/MainActivity.java
+++ app/src/main/java/com/example/antonyng/level_up/view/MainActivity.java
@@ -5,34 +5,13 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-<<<<<<< HEAD
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-<<<<<<< HEAD
 
 import com.example.antonyng.level_up.model.GithubUsers;
-=======
->>>>>>> [Chore] Remove unused imports
-=======
->>>>>>> [Test] Testing
-
 import com.example.antonyng.level_up.R;
 import com.example.antonyng.level_up.presenter.GithubPresenter;
-
-<<<<<<< HEAD
-import java.util.ArrayList;
-import java.util.List;
-<<<<<<< HEAD
-
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
-
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
-=======
->>>>>>> [Chore] Add public function for listing users from api
-
-=======
->>>>>>> [Feature] Remove RecyclerView && pass data to the layout
 
 public class MainActivity extends AppCompatActivity {
 
