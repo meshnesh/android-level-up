b3c19c39983f1455c562fe0d4af194f391de4a83
meshnesh <toonnie.nesh@gmail.com>
2018-07-06 20:00:33 +0300

    [Chore] Fix merge conflicts

--- app/src/main/java/com/example/antonyng/level_up/service/GithubApi.java
+++ app/src/main/java/com/example/antonyng/level_up/service/GithubApi.java
@@ -1,5 +1,6 @@
 package com.example.antonyng.level_up.service;
 
+<<<<<<< HEAD
 import com.example.antonyng.level_up.model.GithubUsersResponse;
 
 import retrofit2.Call;
@@ -10,4 +11,19 @@ public interface GithubApi {
 
     @GET("search/users?q=language:Android+location:nairobi                                                                                                                                                                                                                                                  ")
     Call<GithubUsersResponse> getUserResults();
+=======
+import com.example.antonyng.level_up.model.Data;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface GithubApi {
+
+    @GET("users?")
+    Call<Data> doGetUserList(@Query("page") String page);
+
+    @GET("users/:username")
+    Call<Data> getUserResults();
+>>>>>>> [Chore] Refactor the file name
 }
