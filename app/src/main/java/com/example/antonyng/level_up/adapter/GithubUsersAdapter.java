3192e96c2bdbb7963bcf5f236d4e82c259d761f7
meshnesh <toonnie.nesh@gmail.com>
2018-07-06 18:48:47 +0300

    [Feature] Create Adapter class for the API to pass the data to the main app

---
+++ app/src/main/java/com/example/antonyng/level_up/adapter/GithubUsersAdapter.java
@@ -0,0 +1,88 @@
+package com.example.antonyng.level_up.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.example.antonyng.level_up.view.DetailActivity;
+import com.example.antonyng.level_up.R;
+import com.example.antonyng.level_up.model.GithubUsers;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class GithubUsersAdapter extends RecyclerView.Adapter<GithubUsersAdapter.ViewHolder> {
+
+    private static final String TAG = "GithubUsersAdapter";
+
+    private Context mcontext;
+    private List<GithubUsers> users;
+
+    public GithubUsersAdapter(Context mcontext, List<GithubUsers> users) {
+        this.users = users;
+        this.mcontext = mcontext;
+    }
+
+    @NonNull
+    @Override
+    public GithubUsersAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_listitem, viewGroup, false);
+        return new GithubUsersAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GithubUsersAdapter.ViewHolder viewHolder, int i) {
+        final GithubUsers githubUser = users.get(i);
+
+        Glide.with(mcontext)
+                .asBitmap()
+                .load(githubUser.getAvatarUrl())
+                .into(viewHolder.image);
+
+        viewHolder.imageName.setText(githubUser.getLogin());
+
+        viewHolder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: clicked on:" + githubUser.getLogin());
+
+                Toast.makeText(mcontext, githubUser.getLogin(), Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(mcontext, DetailActivity.class);
+                intent.putExtra("image_url", githubUser.getAvatarUrl());
+                intent.putExtra("dev_name", githubUser.getLogin());
+                mcontext.startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        CircleImageView image;
+        TextView imageName;
+        RelativeLayout parentLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.image);
+            imageName = itemView.findViewById(R.id.image_name);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+        }
+    }
+}
