d32f32b2dc172a1eff6a355ea33e70110b6edbcc
meshnesh <toonnie.nesh@gmail.com>
2018-07-06 19:58:10 +0300

    [Chore] Fix merge conflicts

--- app/src/main/java/com/example/antonyng/level_up/model/GithubUsers.java
+++ app/src/main/java/com/example/antonyng/level_up/model/GithubUsers.java
@@ -4,35 +4,45 @@ import com.google.gson.annotations.SerializedName;
 
 public class GithubUsers {
 
+<<<<<<< HEAD
     @SerializedName("id")
     private String id;
 
+=======
+>>>>>>> [Feature] Create Model directory to user modela classes
     @SerializedName("name")
     private String name;
 
     @SerializedName("email")
     private String email;
 
+<<<<<<< HEAD
     @SerializedName("login")
     private String login;
 
     @SerializedName("avatar_url")
     private String avatar_url;
 
+=======
+>>>>>>> [Feature] Create Model directory to user modela classes
     @SerializedName("company")
     private String company;
 
     @SerializedName("bio")
     private String bio;
 
+<<<<<<< HEAD
     public String getId() {
         return id;
     }
 
+=======
+>>>>>>> [Feature] Create Model directory to user modela classes
     public String getName() {
         return name;
     }
 
+<<<<<<< HEAD
     public String getLogin() {
         return login;
     }
@@ -41,4 +51,17 @@ public class GithubUsers {
         return avatar_url;
     }
 
+=======
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+>>>>>>> [Feature] Create Model directory to user modela classes
 }
--- app/src/main/java/com/example/antonyng/level_up/model/GithubUsersResponse.java
+++ app/src/main/java/com/example/antonyng/level_up/model/GithubUsersResponse.java
@@ -5,10 +5,25 @@ import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 public class GithubUsersResponse {
+<<<<<<< HEAD
     @SerializedName("items")
     public List<GithubUsers> users;
 
     public List<GithubUsers> getUsers() {
         return users;
+=======
+    @SerializedName("messages")
+    private List<String> messages;
+
+    @SerializedName("result")
+    private List<GithubUsers> result;
+
+    public List<GithubUsers> getResult() {
+        return result;
+    }
+
+    public List<String> getMessages() {
+        return messages;
+>>>>>>> [Feature] Create Model directory to user modela classes
     }
 }
