a2ddc619beabd4a2120f9d9c7802584c01cc24e4
meshnesh <toonnie.nesh@gmail.com>
2018-07-09 18:45:45 +0300

    [Fix] Fix conflicting files

--- app/src/main/java/com/example/antonyng/level_up/model/GithubUsers.java
+++ app/src/main/java/com/example/antonyng/level_up/model/GithubUsers.java
@@ -4,58 +4,21 @@ import com.google.gson.annotations.SerializedName;
 
 public class GithubUsers {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     @SerializedName("id")
     private String id;
 
-=======
->>>>>>> [Feature] Create Model directory to user modela classes
-=======
-    @SerializedName("id")
-    private String id;
-
->>>>>>> [Chore] Add user uid=501(antonynganga) gid=20(staff) groups=20(staff),12(everyone),61(localaccounts),79(_appserverusr),80(admin),81(_appserveradm),98(_lpadmin),701(com.apple.sharepoint.group.1),33(_appstore),100(_lpoperator),204(_developer),250(_analyticsusers),395(com.apple.access_ftp),398(com.apple.access_screensharing),399(com.apple.access_ssh) parameter
-    @SerializedName("name")
-    private String name;
-
-    @SerializedName("email")
-    private String email;
-=======
     @SerializedName("login")
     private String login;
 
     @SerializedName("avatar_url")
     private String avatar_url;
->>>>>>> [Chore] Work in progress
 
-    @SerializedName("company")
-    private String company;
-
-<<<<<<< HEAD
-    @SerializedName("bio")
-    private String bio;
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> [Chore] Add user uid=501(antonynganga) gid=20(staff) groups=20(staff),12(everyone),61(localaccounts),79(_appserverusr),80(admin),81(_appserveradm),98(_lpadmin),701(com.apple.sharepoint.group.1),33(_appstore),100(_lpoperator),204(_developer),250(_analyticsusers),395(com.apple.access_ftp),398(com.apple.access_screensharing),399(com.apple.access_ssh) parameter
     public String getId() {
         return id;
     }
 
-<<<<<<< HEAD
-=======
->>>>>>> [Feature] Create Model directory to user modela classes
-=======
->>>>>>> [Chore] Add user uid=501(antonynganga) gid=20(staff) groups=20(staff),12(everyone),61(localaccounts),79(_appserverusr),80(admin),81(_appserveradm),98(_lpadmin),701(com.apple.sharepoint.group.1),33(_appstore),100(_lpoperator),204(_developer),250(_analyticsusers),395(com.apple.access_ftp),398(com.apple.access_screensharing),399(com.apple.access_ssh) parameter
-    public String getName() {
-        return name;
-=======
     public String getLogin() {
         return login;
->>>>>>> [Chore] Work in progress
     }
 
     public String getAvatarUrl() {
--- app/src/main/java/com/example/antonyng/level_up/service/GithubService.java
+++ app/src/main/java/com/example/antonyng/level_up/service/GithubService.java
@@ -1,5 +1,6 @@
 package com.example.antonyng.level_up.service;
 
+
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
@@ -20,24 +21,6 @@ public class GithubService {
 
     public static GithubApi getAPI() {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public GithubApi getAPI() {
-        String BASE_URL = "https://api.github.com/v3/";
-<<<<<<< HEAD
-=======
-    public GithubUsersApi getAPI() {
-=======
-    public GithubApi getAPI() {
->>>>>>> [Feature] Update endpoint for accesing multiple users and single user by username
-        String BASE_URL = "https://api.github.com/";
->>>>>>> [Feature] Create 'GithubService' to handle api base URL && 'GithubUsersApi' to handle HTTP methods
-=======
->>>>>>> [Chore] Update API url for github
-=======
->>>>>>> [Chore] Work in progress
-
         if (retrofit == null) {
             retrofit = new Retrofit
                     .Builder()
@@ -46,18 +29,6 @@ public class GithubService {
                     .client(okHttp.build())
                     .build();
         }
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-<<<<<<< HEAD
         return retrofit.create(GithubApi.class);
-=======
-        return retrofit.create(GithubUsersApi.class);
->>>>>>> [Feature] Create 'GithubService' to handle api base URL && 'GithubUsersApi' to handle HTTP methods
-=======
-=======
->>>>>>> [Chore] Work in progress
-        return retrofit.create(GithubApi.class);
->>>>>>> [Feature] Update endpoint for accesing multiple users and single user by username
     }
 }
