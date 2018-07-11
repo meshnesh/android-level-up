5e35d050d8585fef8307302fd1f0ab177fb1cbf6
meshnesh <toonnie.nesh@gmail.com>
2018-07-11 19:37:08 +0300

    [Feature] Add App configurations

--- app/src/main/java/com/example/antonyng/level_up/presenter/GithubPresenter.java
+++ app/src/main/java/com/example/antonyng/level_up/presenter/GithubPresenter.java
@@ -2,12 +2,14 @@ package com.example.antonyng.level_up.presenter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.example.antonyng.level_up.adapter.GithubUsersAdapter;
 import com.example.antonyng.level_up.model.GithubUsers;
 import com.example.antonyng.level_up.model.GithubUsersResponse;
 import com.example.antonyng.level_up.service.GithubService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -15,21 +17,28 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class GithubPresenter {
+
+    View view;
+
     private Context context;
 
 
-    public GithubPresenter(Context context) {
-        this.context = context;
+    public GithubPresenter(View view) {
+        this.view = view;
     }
 
-    public void getUserList(final RecyclerView recyclerView) {
+    public interface View {
+        void displayGithubUsers(ArrayList<GithubUsers> users);
+    }
+
+    public void getUserList() {
 
         GithubService.getAPI().getUserResults().enqueue(new Callback<GithubUsersResponse>() {
             @Override
             public void onResponse(Call<GithubUsersResponse> call, Response<GithubUsersResponse> response) {
-                List<GithubUsers> users = response.body().getUsers();
+                ArrayList<GithubUsers> users = response.body().getUsers();
                 if (users != null) {
-                    recyclerView.setAdapter(new GithubUsersAdapter(context, users));
+                    view.displayGithubUsers(users);
                 }
             }
 
--- app/src/main/java/com/example/antonyng/level_up/view/MainActivity.java
+++ app/src/main/java/com/example/antonyng/level_up/view/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.antonyng.level_up.view;
 
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,10 +8,18 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import com.example.antonyng.level_up.R;
+import com.example.antonyng.level_up.adapter.GithubUsersAdapter;
+import com.example.antonyng.level_up.model.GithubUsers;
 import com.example.antonyng.level_up.presenter.GithubPresenter;
 
+import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements GithubPresenter.View {
+
+    static final  String ALL_KEYS = "list_state";
+    ArrayList<GithubUsers> users;
+    private final GithubPresenter presenter = new GithubPresenter(this);
 
     private static final String TAG = "MainActivity";
 
@@ -22,13 +31,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            users = savedInstanceState.getParcelableArrayList(ALL_KEYS);
+            displayGithubUsers(users);
+        } else presenter.getUserList();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putParcelableArrayList(ALL_KEYS,(ArrayList<? extends Parcelable>) users);
+    }
+
+    @Override
+    public void displayGithubUsers(ArrayList<GithubUsers> usersList) {
+        users = usersList;
+
         Log.d(TAG, "onCreate: Started");
         mRecyclerView =findViewById(R.id.recycler_view);
         mLayoutManager = new LinearLayoutManager(getApplicationContext());
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-
-        GithubPresenter pres = new GithubPresenter(this);
-        pres.getUserList(mRecyclerView);
+        RecyclerView.Adapter adapter = new GithubUsersAdapter(this, users);
+        mRecyclerView.setAdapter(adapter);
     }
 }
